417d83c7523a190448d451cf4e86523e0e4087e7
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2019-12-27 07:36:49 +1100

    Almost right level of abstraction

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 )
 
 func crash(err error) {
@@ -53,15 +54,39 @@ func Cd(dir string) {
 }
 
 func fetch(url string) {
-	Cd(DistfilesPath)
-	Curl("-O", url)
-	Cd(Cwd)
+	expetedPath := path.Join(DistfilesPath, path.Base(url))
+	if _, err := os.Stat(expetedPath); os.IsNotExist(err) {
+		Cd(DistfilesPath)
+		Curl("-O", url)
+		Cd(Cwd)
+	}
+}
+
+func packageVersion(url string) string {
+	fileName := path.Base(url)
+	//TODO something better in the future
+	endings := []string{
+		".tar.gz",
+		".source.tar.xz", //Hack for firefox
+		".tar.xz",
+		".zip",
+		".tgz",
+		".tar.bz2"}
+
+	var result = fileName
+	for _, ending := range endings {
+		result = strings.TrimSuffix(result, ending)
+	}
+	return result
 }
 
 func extract(url string) {
 	fileName := path.Base(url)
 	tarballPath := path.Join(DistfilesPath, fileName)
 	Tar("xf", tarballPath, "-C", BuildPath)
+
+	extractedPath := path.Join(BuildPath, packageVersion(url))
+	Cd(extractedPath)
 }
 
 // You can think of this as root directory for everything
@@ -80,87 +105,74 @@ func setUpGlobals() {
 	crash(err)
 
 	BuildPath = path.Join(Cwd, "build")
-	os.RemoveAll(BuildPath)
 	err = os.MkdirAll(BuildPath, os.ModePerm)
 	crash(err)
 }
 
-func installGlibc() {
-	url := "http://ftp.gnu.org/gnu/glibc/glibc-2.30.tar.xz"
+func install(url string, build func(string)) {
 	fetch(url)
 	extract(url)
+	sourceDir := path.Join(BuildPath, packageVersion(url))
+	destDir := path.Join(BuildPath, packageVersion(url)+"-package")
+	build(destDir)
 
-	extractedDirName := "glibc-2.30"
-	workDir := path.Join(BuildPath, extractedDirName)
-	destDir := path.Join(BuildPath, extractedDirName+"-package")
-
-	Cd(workDir)
-	Mkdir("build")
-	Cd("build")
-	DotDotConfigure("--prefix=/usr",
-		"--disable-werror",
-		"--enable-kernel=3.2",
-		"--enable-stack-protector=strong",
-		"--with-headers=/usr/include",
-		"libc_cv_slibdir=/lib")
-
-	Make("-j10")
-	Make("install_root="+destDir, "install")
-
+	//clean up and restore cwd
+	Rm("-rf", sourceDir)
+	//Rm("-rf", destDir)
+	Cd(Cwd)
 }
 
 func main() {
 	setUpGlobals()
 
-	installGlibc()
-	Cd(Cwd)
+	install("http://ftp.gnu.org/gnu/glibc/glibc-2.30.tar.xz", func(destDir string) {
+		Mkdir("build")
+		Cd("build")
+		DotDotConfigure("--prefix=/usr",
+			"--disable-werror",
+			"--enable-kernel=3.2",
+			"--enable-stack-protector=strong",
+			"--with-headers=/usr/include",
+			"libc_cv_slibdir=/lib")
 
-	Curl("-O", "http://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.xz")
-	Rm("-rfv", "binutils-2.32")
-	Tar("xf", "binutils-2.32.tar.xz")
-	Cd("binutils-2.32")
-	Mkdir("build")
-	Cd("build")
+		Make("-j10")
+		Make("install_root="+destDir, "install")
+	})
 
-	newRoot := "/home/kirillvr/newroot"
+	install("http://ftp.gnu.org/gnu/binutils/binutils-2.32.tar.xz", func(destDir string) {
+		Mkdir("build")
+		Cd("build")
+		DotDotConfigure("--prefix=/usr",
+			"--enable-gold",
+			"--enable-ld=default",
+			"--enable-plugins",
+			"--enable-shared",
+			"--disable-werror",
+			"--enable-64-bit-bfd",
+			"--with-system-zlib")
+		Make("-j10")
+		Make("install", "DESTDIR="+destDir)
+	})
 
-	DotDotConfigure("--prefix=/usr",
-		"--enable-gold",
-		"--enable-ld=default",
-		"--enable-plugins",
-		"--enable-shared",
-		"--disable-werror",
-		"--enable-64-bit-bfd",
-		"--with-system-zlib")
-	Make("-j10")
-	Make("install", "DESTDIR="+newRoot)
-	Cd("../..")
+	install("http://ftp.gnu.org/gnu/ncurses/ncurses-6.1.tar.gz", func(destDir string) {
+		DotConfigure("--prefix=/usr",
+			"--mandir=/usr/share/man",
+			"--with-shared",
+			"--without-debug",
+			"--without-normal",
+			"--enable-pc-files",
+			"--enable-widec")
+		Make("-j10")
+		Make("install", "DESTDIR="+destDir)
+	})
 
-	Curl("-O", "http://ftp.gnu.org/gnu/ncurses/ncurses-6.1.tar.gz")
-	Rm("-rfv", "ncurses-6.1")
-	Tar("xf", "ncurses-6.1.tar.gz")
-	Cd("ncurses-6.1")
-	DotConfigure("--prefix=/usr",
-		"--mandir=/usr/share/man",
-		"--with-shared",
-		"--without-debug",
-		"--without-normal",
-		"--enable-pc-files",
-		"--enable-widec")
-	Make("-j10")
-	Make("install", "DESTDIR="+newRoot)
-	Cd("..")
+	install("http://ftp.gnu.org/gnu/bash/bash-5.0.tar.gz", func(destDir string) {
+		DotConfigure("--prefix=/usr",
+			"--docdir=/usr/share/doc/bash-5.0",
+			"--without-bash-malloc",
+			"--with-installed-readline")
 
-	Curl("-O", "http://ftp.gnu.org/gnu/bash/bash-5.0.tar.gz")
-	Rm("-rfv", "bash-5.0")
-	Tar("xf", "bash-5.0.tar.gz")
-	Cd("bash-5.0")
-	DotConfigure("--prefix=/usr",
-		"--docdir=/usr/share/doc/bash-5.0",
-		"--without-bash-malloc",
-		"--with-installed-readline")
-
-	Make("-j10")
-	Make("install", "DESTDIR="+newRoot)
-	Cd("..")
+		Make("-j10")
+		Make("install", "DESTDIR="+destDir)
+	})
 }
