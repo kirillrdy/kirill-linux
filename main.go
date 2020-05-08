43da6d5945f8c4d9e923252d6e81198f8313009a
Kirill Radzikhovskyy <kirillrdy@gmail.com>
2020-05-09 08:00:27 +1000

    Extracted env stuff into own package

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
-package main
+package kirill_linux
 
 import (
 	"flag"
 	. "github.com/kirillrdy/kirill-linux/shell"
-	"io"
 	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
 	"path"
 	"strings"
@@ -42,27 +40,6 @@ func appendToFile(fileName string, items ...string) {
 	crash(err)
 }
 
-func (env Env) fetch(url string) {
-	log.Printf("fetching %s\n", url)
-	Cd(env.DistfilesPath)
-
-	// Get the data
-	resp, err := http.Get(url)
-	crash(err)
-	defer resp.Body.Close()
-
-	// Create the file
-	out, err := os.Create(path.Base(url))
-	crash(err)
-	defer out.Close()
-
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	crash(err)
-
-	Cd(env.Cwd)
-}
-
 func packageVersion(url string) string {
 	fileName := path.Base(url)
 	//TODO something better in the future
@@ -81,107 +58,7 @@ func packageVersion(url string) string {
 	return result
 }
 
-func (env Env) extract(url string) {
-
-	expetedTarPath := path.Join(env.DistfilesPath, path.Base(url))
-	if _, err := os.Stat(expetedTarPath); os.IsNotExist(err) {
-		env.fetch(url)
-	}
-
-	extractedSourcePath := path.Join(env.BuildPath, packageVersion(url))
-	if _, err := os.Stat(extractedSourcePath); os.IsNotExist(err) {
-		tarballPath := path.Join(env.DistfilesPath, path.Base(url))
-		Tar("xf", tarballPath, "-C", env.BuildPath)
-	}
-
-	Cd(extractedSourcePath)
-}
-
-type Env struct {
-	// Cwd we remeber cwd in order to get back here
-	Cwd string
-
-	// DistfilesPath is where all distfiles are stored
-	DistfilesPath string
-
-	// BuildPath is where all the work is done
-	BuildPath string
-
-	// PkgPath is where binary packages get stored
-	PkgPath string
-
-	// InstallPrefix prefix is where we are all going to install things for new system
-	InstallPrefix string
-}
-
-func (env Env) setUpGlobals() {
-
-	var err error
-	env.Cwd, err = os.Getwd()
-	crash(err)
-
-	env.DistfilesPath = path.Join(env.Cwd, "distfiles")
-	err = os.MkdirAll(env.DistfilesPath, os.ModePerm)
-	crash(err)
-
-	env.BuildPath = path.Join(env.Cwd, "build")
-	err = os.MkdirAll(env.BuildPath, os.ModePerm)
-	crash(err)
-
-	env.PkgPath = path.Join(env.Cwd, "package")
-	err = os.MkdirAll(env.PkgPath, os.ModePerm)
-	crash(err)
-
-	env.InstallPrefix = path.Join(env.Cwd, "newroot")
-	err = os.MkdirAll(env.InstallPrefix, os.ModePerm)
-	crash(err)
-
-}
-
-func (env Env) installSimple(url string) {
-	env.installConfigure(url, func() {
-		DotConfigure("--prefix=/usr")
-	})
-}
-
-//TODO detect
-const NumberOfMakeJobs = "-j8"
-
-func (env Env) installConfigure(url string, configure func()) {
-	env.installBuildInstall(url, func() {
-		configure()
-
-		Make(NumberOfMakeJobs)
-	}, func(destDir string) {
-		Make("install", "DESTDIR="+destDir)
-	})
-}
-
-// stupid name, but what can you do
-func (env Env) installBuildInstall(url string, build func(), install func(string)) {
-	tarBall := path.Join(env.PkgPath, packageVersion(url)+".tar.xz")
-
-	if _, err := os.Stat(tarBall); os.IsNotExist(err) {
-		env.extract(url)
-		sourceDir := path.Join(env.BuildPath, packageVersion(url))
-
-		build()
-
-		// Part of packaging
-		destDir := path.Join(env.BuildPath, packageVersion(url)+"-package")
-		install(destDir)
-		Tar("cf", tarBall, "-C", destDir, ".")
-		Cd(env.Cwd)
-		Rm("-rf", destDir)
-		Rm("-rf", sourceDir)
-	}
-
-	//TODO also dont do this if its already installed eg need some way of tracking those
-	//TODO replace with desired prefix
-	Tar("xf", tarBall, "-C", env.InstallPrefix)
-}
-
-func main() {
+func mainFunction() {
 	env := Env{}
 	env.setUpGlobals()
 
